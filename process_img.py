7fce4b9893f7f55d629f27ed42c7d98dd7982af3
sai3010 <saiprasanth2007@gmail.com>
2020-10-17 11:32:48 +0530

    added comments in the code

--- process_img.py
+++ process_img.py
@@ -1,17 +1,25 @@
 import numpy as np
 import cv2
 
+# importing the image through opencv
 image = cv2.imread('img.png')
+# using GaussianBlur to remove noise from the image
 blurry = cv2.GaussianBlur(image,(5,5),0)
+# Converting to HSV for object detection
 hsv = cv2.cvtColor(blurry, cv2.COLOR_BGR2HSV)
+
 wifi_area = ["Manager's office","Office Area B","Waiting Room","Lift lobby","Conference Room","Office Area A"]
 ble_area = ["Manager's office","Office Area B","Office Area A","Conference Room"]
+
 def wifi(dev_id):
     wifi_list =[]
+    # specifying the uppper and lower range of red colour.
     low_red = np.array([161,155,84])
     high_red = np.array([179,255,255])
+    # detecting the boundary 
     mask = cv2.inRange(hsv, low_red, high_red)
     ret,thresh = cv2.threshold(mask,127,255,0)
+    # finding white objects in black bg using contours
     contours, hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
     j=0
     for c in contours:
@@ -28,10 +36,13 @@ def wifi(dev_id):
 
 def ble(dev_id):
     ble_list =[]
+    # specifying the uppper and lower range of red colour.
     low_blue = np.array([94,80,2])
     high_blue = np.array([126,255,255])
+    # detecting the boundary 
     mask = cv2.inRange(hsv, low_blue, high_blue)
     ret,thresh = cv2.threshold(mask,127,255,0)
+    # finding white objects in black bg using contours
     contours, hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
     j=0
     for c in contours:
